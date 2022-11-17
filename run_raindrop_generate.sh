5e461558ec93537d6c4c4e715137e2e3e7bb9a5f
Fazry <lhuqita.fazry@gmail.com>
2022-11-17 12:35:05 +0700

    fixing bug

--- run_raindrop_generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p datasets/RainDropS
-drop_generator/makeRain/build/Test datasets/leftImg8bit datasets/RainDropS
--- run_raindrop_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt install liblapack-dev
-apt install libblas-dev
-apt install libboost-dev
-apt install libarmadillo-dev
-apt install libjsoncpp-dev
-apt install libopencv-dev python3-opencv
