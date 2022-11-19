fc5453496d602ea5361ce21f044d5faa6b72e510
Fazry <lhuqita.fazry@gmail.com>
2022-11-19 14:59:40 +0700

    fixing bug

--- run_predict_raindrop_detector.sh
+++ run_predict_raindrop_detector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=$1 python raindrop_detector.py \
     --mode=predict \
-    --data_dir=datasets/RainDropS/test_a \
+    --data_dir=datasets/RainDrop/test_a \
     --batch_size=32 \
     --num_workers=4 \
     --accelerator=gpu
