2d99bef28d4d3170e6fcb71e78e6a2ee30963156
Fazry <lhuqita.fazry@gmail.com>
2022-11-20 10:27:50 +0700

    fixing bug

--- datasets/RaindropDataset.py
+++ datasets/RaindropDataset.py
@@ -45,7 +45,7 @@ class RaindropDataset(torch.utils.data.Dataset):
         if self.masks is None:
             image = center_crop(image, (128, 128))
             image = image.transpose(2, 0, 1).astype('float32')
-            image = (image - 128) / 128.
+            image /= 256.
 
             return image
 
@@ -60,7 +60,7 @@ class RaindropDataset(torch.utils.data.Dataset):
         mask = np.expand_dims(mask, axis=0)
         
         image = image.transpose(2, 0, 1).astype('float32')
-        image = (image - 128) / 128.
+        image /= 256.
 
         return image, mask
             
