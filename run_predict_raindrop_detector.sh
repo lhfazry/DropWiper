583524fc67c8450f4581aa714cf137ddb635a461
Fazry <lhuqita.fazry@gmail.com>
2022-11-20 12:36:28 +0700

    fixing bug

--- run_predict_raindrop_detector.sh
+++ run_predict_raindrop_detector.sh
@@ -2,6 +2,7 @@
 CUDA_VISIBLE_DEVICES=$1 python raindrop_detector.py \
     --mode=predict \
     --data_dir=datasets/RainDrop/test_a \
+    --ckpt_path=lightning_logs/raindrop_detector/version_5/checkpoints/epoch\=40-step\=25625.ckpt \
     --batch_size=32 \
     --num_workers=4 \
     --accelerator=gpu
