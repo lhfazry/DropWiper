10ba44840ef703ddbbed11e7b34f2f0b4f595cc8
Fazry <lhuqita.fazry@gmail.com>
2022-11-20 15:31:16 +0700

    fixing bug

--- models/raindrop_detector.py
+++ models/raindrop_detector.py
@@ -53,15 +53,16 @@ class RaindropDetector(pl.LightningModule):
 
     def configure_optimizers(self):
         # define optimizer
-        optimizer = torch.optim.Adam([ 
-            dict(params=self.parameters(), lr=0.0008),
-        ])
+        #optimizer = torch.optim.Adam([ 
+        #    dict(params=self.parameters(), lr=0.001, weight_decay=0.00001),
+        #])
 
-        # define learning rate scheduler (not used in this NB)
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-            optimizer, T_0=1, T_mult=2, eta_min=5e-5,
-        )
-        #optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4, weight_decay=1e-4)
-        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.85, verbose=True)
+        #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        #    optimizer, T_0=1, T_mult=2, eta_min=5e-5,
+        #)
+
+        #torch.optim.lr_scheduler.
+        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4, weight_decay=1e-4)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.85, verbose=True)
 
         return [optimizer], [lr_scheduler]
